c90d748007e489f5a5e1f0b34afb5168ae99fbf8
Ariel Avi <avi.ariel.g@gmail.com>
2023-12-07 13:07:25 +0100

    Implemented ranked selection unit tests for 1-4 ranks

--- tests/src/genetic_algorithm_tests.cpp
+++ tests/src/genetic_algorithm_tests.cpp
@@ -1,6 +1,7 @@
 
 #include <gtest/gtest.h>
 #include <source_location>
+#include <ranges>
 #include "genetic_algorithm_base.h"
 
 using namespace minimacore::genetic_algorithm;
@@ -13,15 +14,44 @@ protected:
   
   void SetUp() override
   {
+    unique_sorted_ranks = this->ranks;
+    std::sort(unique_sorted_ranks.begin(), unique_sorted_ranks.end());
+    auto end = std::unique(unique_sorted_ranks.begin(), unique_sorted_ranks.end());
+    unique_sorted_ranks.erase(end, unique_sorted_ranks.end());
+    unique_sorted_ranks.shrink_to_fit();
     for (size_t i = 0; i < 10; i++) {
-      auto& ind = _population.emplace_back(std::make_unique<individual_impl>(Eigen::VectorX<F>(3)));
-      ind->append_fitness_value(_fitness_values[0][i]);
-      ind->append_fitness_value(_fitness_values[1][i]);
+      auto& ind = _population.emplace_back(std::make_unique<individual_impl>(Eigen::VectorX<F>(3), 2));
+      ind->set_fitness_value(0, _fitness_values[0][i]);
+      ind->set_fitness_value(1, _fitness_values[1][i]);
     }
+  }
+  
+  void test_ranked_selection_by_rank(size_t rank_count)
+  {
+    vector<base_individual<F>*> test_set;
+    for (auto& rank_i : unique_sorted_ranks) {
+      if (rank_i < rank_count) {
+        for (size_t i{0}; i < ranks.size(); i++) {
+          if (ranks[i] == rank_i) test_set.push_back(_population[i].get());
+        }
+      }
+    }
+    
+    ranked_selection_for_reproduction<F> selection_for_reproduction(
+        rank_count, ranked_selection_for_reproduction<F>::select_by_ranks);
+    vector top_rank = selection_for_reproduction(_population);
+    ASSERT_EQ(top_rank.size(), test_set.size()) << "Rank count: " << rank_count;
+    
+    std::ranges::for_each(top_rank,
+                          [test_set](auto& individual) {
+                            EXPECT_TRUE(std::find(test_set.begin(), test_set.end(), individual) != test_set.end())
+                                      << "Couldn't find " << individual << " in test set.";
+                          });
   }
   
   vector<individual_ptr<F>> _population;
   
+  // Two objectives (fitness values) for each individual in the population, to constitute multi-objectiveness
   vector<vector<F>> _fitness_values{
       {
           {
@@ -51,6 +81,7 @@ protected:
       }
   };
   
+  // These are the ranks of the objective functions defined above - taken manually for check against unit test
   vector<size_t> ranks{
       {
           1,
@@ -65,6 +96,8 @@ protected:
           2
       }
   };
+  
+  vector<size_t> unique_sorted_ranks;
   
 };
 
@@ -86,12 +119,13 @@ TYPED_TEST(minimacore_genetic_algorithm_tests, tournament_selection_for_reproduc
   tournament_selection_for_reproduction<TypeParam> selection(tournament_size, selection_size);
   vector selected_individuals = selection(this->_population);
   ASSERT_EQ(selected_individuals.size(), selection_size);
-  for (auto& i : selected_individuals) {
-    ASSERT_NE(i, this->_population[5].get());
-  }
+  for (auto& i : selected_individuals) ASSERT_NE(i, this->_population[5].get());
 }
 
 TYPED_TEST(minimacore_genetic_algorithm_tests, ranked_selection_for_reproduction)
 {
-  // todo
+  this->test_ranked_selection_by_rank(1);
+  this->test_ranked_selection_by_rank(2);
+  this->test_ranked_selection_by_rank(3);
+  this->test_ranked_selection_by_rank(4);
 }

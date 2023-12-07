e192dc1fd4a1863bb68ee543af2685e801299e61
Ariel Avi <avi.ariel.g@gmail.com>
2023-12-07 13:07:29 +0100

    Reverted coverage tool to lcov so it doesn't report partial coverage

--- ci/coverage.sh
+++ ci/coverage.sh
@@ -1,2 +1,6 @@
+cmake -S . -B build/coverage -DCMAKE_BUILD_TYPE="Coverage"
+cmake --build build/coverage --config Debug --target minimacore_tests
+lcov --capture --no-external --initial --directory . --output-file coverage_baseline.info --exclude '/usr/*' --exclude '*/tests/src/*'
 ./build/coverage/tests/minimacore_tests
-gcovr --filter='src/' --xml-pretty > coverage.xml
+lcov --capture --directory . --output-file coverage_total.info --exclude '/usr/*' --exclude '*/tests/src/*'
+lcov -a coverage_baseline.info -a coverage_total.info -o coverage.info

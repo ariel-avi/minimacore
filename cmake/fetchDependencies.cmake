25a8d8982c8e42236549230ae6f7484689ef4170
Ariel Avi <avi.ariel.g@gmail.com>
2023-12-07 13:07:26 +0100

    Changed GIT_REPO to GIT_REPOSITORY

--- cmake/fetchDependencies.cmake
+++ cmake/fetchDependencies.cmake
@@ -1,6 +1,6 @@
 include(FetchContent)
 fetchcontent_declare(Eigen3
-        GIT_REPO https://gitlab.com/libeigen/eigen.git
+        GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
         GIT_TAG e7248b26a1ed53fa030c5c459f7ea095dfd276ac
         FIND_PACKAGE_ARGS)
 fetchcontent_makeavailable(Eigen3)

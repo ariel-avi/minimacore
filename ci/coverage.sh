940a6059e62cbf98a8befacbaa82874a58097477
Ariel Avi <avi.ariel.g@gmail.com>
2023-12-07 13:07:30 +0100

    Added builds/_deps to coverage exclusion

--- ci/coverage.sh
+++ ci/coverage.sh
@@ -1,6 +1,6 @@
 cmake -S . -B build/coverage -DCMAKE_BUILD_TYPE="Coverage"
 cmake --build build/coverage --config Debug --target minimacore_tests
-lcov --capture --no-external --initial --directory . --output-file coverage_baseline.info --exclude '/usr/*' --exclude '*/tests/src/*'
+lcov --capture --no-external --initial --directory . --output-file coverage_baseline.info --exclude '/usr/*' --exclude '*/tests/src/*' --exclude '*/build/_deps/*'
 ./build/coverage/tests/minimacore_tests
-lcov --capture --directory . --output-file coverage_total.info --exclude '/usr/*' --exclude '*/tests/src/*'
+lcov --capture --directory . --output-file coverage_total.info --exclude '/usr/*' --exclude '*/tests/src/*' --exclude '*/build/_deps/*'
 lcov -a coverage_baseline.info -a coverage_total.info -o coverage.info
